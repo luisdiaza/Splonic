114e91b30d67981cb6f80e7f0bfdb920dfabd493
Luis Diaz <diazluis1048@gmail.com>
2017-07-10 14:44:53 -0400

    made navbar clickable and added some scrollreveal functionality

--- script.js
+++ script.js
@@ -2,6 +2,29 @@
 
 window.sr = ScrollReveal();//instantiates ScrollReveal
 
+sr.reveal('.sonic', {
+  duration: 1000,
+  origin:'left',
+  distance:'300px',
+  viewFactor: 0.2
+});
+
+sr.reveal('#CallToAction h1', {
+  duration: 2000,
+  delay: 500,
+  origin:'right',
+  distance:'300px',
+  viewFactor: 0.2
+});
+
+sr.reveal('.flipper-container', {
+  duration: 4000,
+  delay: 500,
+  origin:'top',
+  distance:'300px',
+  viewFactor: 0.2
+});
+
 /* Modal */
 
 // Get the modal
