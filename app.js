489272f3ae945c86c1458224e519d4606d82081f
Luis Diaz <diazluis1048@gmail.com>
2018-01-22 02:45:56 -0500

    implemented vertical smooth scrolling using scrollIntoView JS function and removed Scroll Reveal animations

--- app.js
+++ app.js
@@ -1,54 +1,64 @@
 /* ScrollReveal */
 
-window.sr = ScrollReveal();//instantiates ScrollReveal
-
-sr.reveal('.sonic', {
-  duration: 1000,
-  origin:'left',
-  distance:'300px',
-  viewFactor: 0.2
-});
-
-sr.reveal('#CallToAction h1', {
-  duration: 2000,
-  delay: 500,
-  origin:'right',
-  distance:'300px',
-  viewFactor: 0.2
-});
-
-sr.reveal('.flipper-container', {
-  duration: 4000,
-  delay: 500,
-  origin:'top',
-  distance:'300px',
-  viewFactor: 0.2
-});
+// window.sr = ScrollReveal();//instantiates ScrollReveal
+//
+// sr.reveal('.sonic', {
+//   duration: 1000,
+//   origin:'left',
+//   distance:'300px',
+//   viewFactor: 0.2
+// });
+//
+// sr.reveal('#CallToAction h1', {
+//   duration: 2000,
+//   delay: 500,
+//   origin:'right',
+//   distance:'300px',
+//   viewFactor: 0.2
+// });
+//
+// sr.reveal('.flipper-container', {
+//   duration: 4000,
+//   delay: 500,
+//   origin:'top',
+//   distance:'300px',
+//   viewFactor: 0.2
+// });
 
 //Define UI variables
-const arrow = document.querySelector('.flipper-container')
+const callToAction = document.querySelector('.CallToAction');
+const entertainment = document.querySelector('.entertainment');
+const artistes = document.querySelector('.art');
+const arrow = document.querySelector('.flipper-container');
+const unorderedList = document.querySelector('.unorderedList');
+/* Event Handlers*/
 
-//Listen for open click
+// Scroll to CallToAction section when RSVP is clicked in nav bar
+unorderedList.addEventListener('click', function(e){
+    if(e.target.className === 'rsvp') {
+        callToAction.scrollIntoView({behavior: "smooth"});
+    }
+});
+
+// Scroll to entertainment section when entertainment section is clicked in nav bar
+unorderedList.addEventListener('click', function(e){
+    if(e.target.className === 'performers') {
+        entertainment.scrollIntoView({behavior: "smooth"});
+    }
+});
+
+// Scroll to art section when artists is clicked in nav bar
+unorderedList.addEventListener('click', function(e){
+    if(e.target.className === 'artistes') {
+        artistes.scrollIntoView({behavior: "smooth"});
+    }
+});
+
+//Opens modal when RSVP button is clicked
 arrow.addEventListener('click',function(e) {
     console.log(e.target.className);
-    if(e.target.className === "front-face"){
+    if(e.target.className === "front-face") {
         //window.location.reload();
         alert("Hello, I am an alert!");
     }
 });
-
-/*$(function() {
-  // Smooth Scrolling
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
-});*/
